37675b750aab3cda35b11c1b365227faf08f7177
박준우(Junwoo Park)/BotService개발팀/SKP <skp.1003874@sk.com>
2018-09-18 23:03:22 +0900

    fix train.py args and print iter log

--- train.py
+++ train.py
@@ -11,16 +11,17 @@ parser.add_argument('--in_ngc', type=int, default=3, help='input channel for gen
 parser.add_argument('--out_ngc', type=int, default=3, help='output channel for generator')
 parser.add_argument('--in_ndc', type=int, default=3, help='input channel for discriminator')
 parser.add_argument('--out_ndc', type=int, default=1, help='output channel for discriminator')
-parser.add_argument('--batch_size', type=int, default=512, help='batch size')
+parser.add_argument('--batch_size', type=int, default=8, help='batch size')
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
 parser.add_argument('--nb', type=int, default=8, help='the number of resnet block layers for generator')
-parser.add_argument('--img_size', type=int, default=64, help='input image size')
-parser.add_argument('--train_epoch', type=int, default=100)
+parser.add_argument('--img_size', type=int, default=128, help='input image size')
+parser.add_argument('--train_epoch', type=int, default=20)
 parser.add_argument('--lrD', type=float, default=0.0002, help='learning rate, default=0.0002')
 parser.add_argument('--lrG', type=float, default=0.0002, help='learning rate, default=0.0002')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for Adam optimizer')
 parser.add_argument('--beta2', type=float, default=0.999, help='beta2 for Adam optimizer')
+parser.add_argument('--print_freq', type=int, default=10, help='print image translation results')
 args = parser.parse_args()
 
 print('------------ Options -------------')
@@ -33,13 +34,14 @@ if torch.backends.cudnn.enabled:
     torch.backends.cudnn.benchmark = True
 
 # results save path
-if not os.path.isdir(os.path.join(args.dataset_name + '_results', 'img')):
-    os.makedirs(os.path.join(args.dataset_name + '_results', 'img'))
-if not os.path.isdir(os.path.join(args.dataset_name + '_results', 'model')):
-    os.makedirs(os.path.join(args.dataset_name + '_results', 'model'))
+if not os.path.isdir(os.path.join(args.dataset + '_results', 'img')):
+    os.makedirs(os.path.join(args.dataset + '_results', 'img'))
+if not os.path.isdir(os.path.join(args.dataset + '_results', 'model')):
+    os.makedirs(os.path.join(args.dataset + '_results', 'model'))
 
 # data_loader
 transform = transforms.Compose([
+        transforms.CenterCrop(178),
         transforms.Resize((args.img_size, args.img_size)),
         transforms.ToTensor(),
         transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
@@ -49,6 +51,12 @@ train_loader_B = utils.data_load(os.path.join('data', args.dataset), 'trainB', t
 test_loader_A = utils.data_load(os.path.join('data', args.dataset), 'testA', transform, 1, shuffle=True, drop_last=True)
 test_loader_B = utils.data_load(os.path.join('data', args.dataset), 'testB', transform, 1, shuffle=True, drop_last=True)
 
+print('------------ Datasets -------------')
+print('TrainA:', len(train_loader_A))
+print('TrainB:', len(train_loader_B))
+print('TestA:', len(test_loader_A))
+print('TestB:', len(test_loader_B))
+print('-------------- End ----------------')
 # network
 En_A = networks.encoder(in_nc=args.in_ngc, nf=args.ngf, img_size=args.img_size).to(device)
 En_B = networks.encoder(in_nc=args.in_ngc, nf=args.ngf, img_size=args.img_size).to(device)
@@ -178,12 +186,44 @@ for epoch in range(args.train_epoch):
 
         iter += 1
 
+        per_epoch_time = time.time() - epoch_start_time
+        train_hist['per_epoch_time'].append(per_epoch_time)
+        print(
+            '[%d][%d/%d] - time: %.2f, Disc A loss: %.3f, Disc B loss: %.3f, Gen loss: %.3f' % (
+                (epoch + 1), iter, min(len(train_loader_A),len(train_loader_B)), per_epoch_time, torch.mean(torch.FloatTensor(Disc_A_losses)),
+                torch.mean(torch.FloatTensor(Disc_B_losses)), torch.mean(torch.FloatTensor(Gen_losses)),))
+
+        if iter % args.print_freq == 0:
+            with torch.no_grad():
+                En_A.eval()
+                En_B.eval()
+                De_A.eval()
+                De_B.eval()
+                n = 0
+                for (A, _), (B, _) in zip(test_loader_A, test_loader_B):
+                    A, B = A.to(device), B.to(device)
+
+                    in_A, sp_A = En_A(A)
+                    in_B, sp_B = En_B(B)
+
+                    B2A = De_A(in_B + sp_A)
+                    A2B = De_B(in_A + sp_B)
+
+                    result = torch.cat((A[0], B[0], A2B[0], B2A[0]), 2)
+                    path = os.path.join(args.dataset + '_results', 'img',
+                                        str(iter + 1) + '_epoch_' + args.dataset + '_' + str(n + 1) + '.png')
+                    plt.imsave(path, (result.cpu().numpy().transpose(1, 2, 0) + 1) / 2)
+                    break
+
+
+
     per_epoch_time = time.time() - epoch_start_time
     train_hist['per_epoch_time'].append(per_epoch_time)
     print(
         '[%d/%d] - time: %.2f, Disc A loss: %.3f, Disc B loss: %.3f, Gen loss: %.3f' % (
             (epoch + 1), args.train_epoch, per_epoch_time, torch.mean(torch.FloatTensor(Disc_A_losses)),
             torch.mean(torch.FloatTensor(Disc_B_losses)), torch.mean(torch.FloatTensor(Gen_losses)),))
+
 
 
     with torch.no_grad():
@@ -202,36 +242,36 @@ for epoch in range(args.train_epoch):
             A2B = De_B(in_A + sp_B)
 
             result = torch.cat((A[0], B[0], A2B[0], B2A[0]), 2)
-            path = os.path.join(args.dataset_name + '_results', 'img', str(epoch+1) + '_epoch_' + args.dataset_name + '_' + str(n + 1) + '.png')
+            path = os.path.join(args.dataset + '_results', 'img', str(epoch+1) + '_epoch_' + args.dataset + '_' + str(n + 1) + '.png')
             plt.imsave(path, (result.cpu().numpy().transpose(1, 2, 0) + 1) / 2)
             n += 1
 
-        torch.save(En_A.state_dict(), os.path.join(args.dataset_name + '_results', 'model', 'En_A_param_latest.pkl'))
-        torch.save(En_B.state_dict(), os.path.join(args.dataset_name + '_results', 'model', 'En_B_param_latest.pkl'))
-        torch.save(De_A.state_dict(), os.path.join(args.dataset_name + '_results', 'model', 'De_A_param_latest.pkl'))
-        torch.save(De_B.state_dict(), os.path.join(args.dataset_name + '_results', 'model', 'De_B_param_latest.pkl'))
-        torch.save(Disc_A.state_dict(), os.path.join(args.dataset_name + '_results', 'model', 'Disc_A_param_latest.pkl'))
-        torch.save(Disc_B.state_dict(), os.path.join(args.dataset_name + '_results', 'model', 'Disc_B_param_latest.pkl'))
+        torch.save(En_A.state_dict(), os.path.join(args.dataset + '_results', 'model', 'En_A_param_latest.pkl'))
+        torch.save(En_B.state_dict(), os.path.join(args.dataset + '_results', 'model', 'En_B_param_latest.pkl'))
+        torch.save(De_A.state_dict(), os.path.join(args.dataset + '_results', 'model', 'De_A_param_latest.pkl'))
+        torch.save(De_B.state_dict(), os.path.join(args.dataset + '_results', 'model', 'De_B_param_latest.pkl'))
+        torch.save(Disc_A.state_dict(), os.path.join(args.dataset + '_results', 'model', 'Disc_A_param_latest.pkl'))
+        torch.save(Disc_B.state_dict(), os.path.join(args.dataset + '_results', 'model', 'Disc_B_param_latest.pkl'))
 
 
         if (epoch+1) % 50 == 0:
             torch.save(En_A.state_dict(),
-                       os.path.join(args.dataset_name + '_results', 'model', 'En_A_param_' + str(epoch+1) + '.pkl'))
+                       os.path.join(args.dataset + '_results', 'model', 'En_A_param_' + str(epoch+1) + '.pkl'))
             torch.save(En_B.state_dict(),
-                       os.path.join(args.dataset_name + '_results', 'model', 'En_B_param_' + str(epoch+1) + '.pkl'))
+                       os.path.join(args.dataset + '_results', 'model', 'En_B_param_' + str(epoch+1) + '.pkl'))
             torch.save(De_A.state_dict(),
-                       os.path.join(args.dataset_name + '_results', 'model', 'De_A_param_' + str(epoch+1) + '.pkl'))
+                       os.path.join(args.dataset + '_results', 'model', 'De_A_param_' + str(epoch+1) + '.pkl'))
             torch.save(De_B.state_dict(),
-                       os.path.join(args.dataset_name + '_results', 'model', 'De_B_param_' + str(epoch+1) + '.pkl'))
+                       os.path.join(args.dataset + '_results', 'model', 'De_B_param_' + str(epoch+1) + '.pkl'))
             torch.save(Disc_A.state_dict(),
-                       os.path.join(args.dataset_name + '_results', 'model', 'Disc_A_param_' + str(epoch+1) + '.pkl'))
+                       os.path.join(args.dataset + '_results', 'model', 'Disc_A_param_' + str(epoch+1) + '.pkl'))
             torch.save(Disc_B.state_dict(),
-                       os.path.join(args.dataset_name + '_results', 'model', 'Disc_B_param_' + str(epoch+1) + '.pkl'))
+                       os.path.join(args.dataset + '_results', 'model', 'Disc_B_param_' + str(epoch+1) + '.pkl'))
 
 total_time = time.time() - start_time
 train_hist['total_time'].append(total_time)
 
 print("Avg one epoch time: %.2f, total %d epochs time: %.2f" % (torch.mean(torch.FloatTensor(train_hist['per_epoch_time'])), args.train_epoch, total_time))
 print("Training finish!... save training results")
-with open(os.path.join(args.dataset_name + '_results',  'train_hist.pkl'), 'wb') as f:
+with open(os.path.join(args.dataset + '_results',  'train_hist.pkl'), 'wb') as f:
     pickle.dump(train_hist, f)
